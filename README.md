a2f13f1cd1143633d2010492cf962ba96fd76cfb
Jordan Scales <scalesjordan@gmail.com>
2014-05-18 15:22:52 -0400

    individual directories for each post

--- README.md
+++ README.md
@@ -4,7 +4,4 @@ A static site generator for lazy people
 
 ### TODO
 
-*In a given directory*
-Read `posts/` directory
-Parse them into HTML
-Throw them in `build/`
+* Static files
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp');
 var markdown = require('gulp-markdown');
+var rename = require('gulp-rename');
 
 /* Where do we find `posts/` */
 gulp.src('posts/*.md')
   .pipe(markdown())
+  .pipe(rename(function (path) {
+    path.dirname = '/' + path.basename;
+    path.basename = 'index';
+  }))
   .pipe(gulp.dest('build/'))
